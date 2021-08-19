19bad64cb2c951e02688345844e0cd2407594b43
RameshBabu.B <RameshBabu.B@brillio.com>
2021-08-19 10:17:13 +0530

    minor changes in endpoint

--- spring-reactive-crud/spring-reactive-crud/src/main/java/com/learn/springreactivecrud/controller/EmployeeController.java
+++ spring-reactive-crud/spring-reactive-crud/src/main/java/com/learn/springreactivecrud/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/v1/employee")
 public class EmployeeController {
 
     private EmployeeService service;
